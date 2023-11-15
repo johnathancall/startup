c8dc907f01f757b25b382e87bc6be56470487f00
Johnathan <johnathancall@gmail.com>
2023-11-15 15:48:11 -0700

    Added references to third-party endpoints: namely, referencing quote and picture generators in the 'about' page.

--- login.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  const nameEl = document.querySelector("#username");
-  localStorage.setItem("username", nameEl.value);
-
-  const passEl = document.querySelector("#password");
-  localStorage.setItem("password", passEl.value);
-
-  window.location.href = "websites.html";
-}
