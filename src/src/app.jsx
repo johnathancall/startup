e9fee2359793571267b7b9a4f7cc25c3b82ad570
Johnathan <johnathancall@gmail.com>
2023-12-12 17:47:00 -0700

    Navbar now goes to functional endpoints.

--- src/src/app.jsx
+++ src/src/app.jsx
@@ -1,45 +1,63 @@
+import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
+
+import {Login} from './login/login';
+import {Websites} from './websites/websites';
+import {About} from './about/about';
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
   return (
-    <div className='body bg-dark text-light'>
-      <header className='container-fluid'>
-        <nav className='navbar fixed-top navbar-dark'>
-          <div className='navbar-brand'>
-            Bookmarker++<sup>&reg;</sup>
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              Bookmarker++<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Login
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='Websites'>
+                  Websites
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='About'>
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
+
+	<Routes>
+	  <Route path='/' element={<Login />} exact />
+	  <Route path='/websites' element={<Websites />} />
+  	  <Route path='/about' element={<About />} />
+  	  <Route path='*' element={<NotFound />} /> 
+	</Routes>
+
+
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Johnathan Call</span>
+            <a className='text-reset' href='https://github.com/johnathancall/startup'>
+              Source
+            </a>
           </div>
-          <menu className='navbar-nav'>
-            <li className='nav-item'>
-              <a className='nav-link' href='index.html'>
-                Home
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='websites.html'>
-                Scores
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='about.html'>
-                About
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
-
-      <main>App components go here</main>
-
-      <footer className='bg-dark text-white-50'>
-        <div className='container-fluid'>
-          <span className='text-reset'>Johnathan Call</span>
-          <a className='text-reset' href='https://github.com/johnathancall/startup'>
-            Source
-          </a>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
---
+++ src/src/websites/websites.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Websites() {
+	return(
+		<main className='container-fluid bg-secondary text-center'>
+			<div>Websites displayed here</div>
+		</main>
+	);
+}
